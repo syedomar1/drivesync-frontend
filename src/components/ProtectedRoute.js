3a889c399f55ae442b32878a025ff2195109b1c6
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-08-13 06:33:29 +0530

    Added ProtectRoute

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ element: Component, requiredRole }) => {
+  const isAdmin = localStorage.getItem("isAdmin") === "true";
+  const userRole = localStorage.getItem("userRole");
+
+  // Redirect to login if not authenticated or if role does not match
+  if (!isAdmin || (requiredRole && userRole !== requiredRole)) {
+    return <Navigate to="/login" />;
+  }
+
+  return Component;
+};
+
+export default ProtectedRoute;
