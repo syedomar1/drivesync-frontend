dae2eb11406b3be252461c26c9c17e848c6e00ab
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-10-28 16:30:46 +0530

    Added link to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
   return (
     <nav className="bg-purple-900 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-2xl font-bold">DriveSync</div>
+      <Link to="/"><div className="text-2xl font-bold">DriveSync</div></Link>
         <div className="hidden md:flex space-x-6">
           <Link to="/" className="hover:text-lightPurple">Home</Link>
           <Link to="/driverform" className="hover:text-lightPurple">Driver Addition</Link>
